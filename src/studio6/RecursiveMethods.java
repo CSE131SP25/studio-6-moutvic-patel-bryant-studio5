c8e649f4ba0ccbed486059e79e9608ba0e9ed5db
laurenmoutvic <laurenmoutvic@localhost>
2025-03-05 15:47:00 -0600

    bikini boy = Doug Shook

--- src/studio6/RecursiveMethods.java
+++ src/studio6/RecursiveMethods.java
@@ -14,7 +14,11 @@ public class RecursiveMethods {
 	public static double geometricSum(int n) {
 		
 			// FIXME compute the geometric sum for the first n terms recursively
-			return 0;
+			if(n == 0) {		//must ensure that we always reach this base case (ex: n-1)
+				return 0;
+			} else {
+				return Math.pow(0.5, n) + geometricSum(n - 1);
+			}	
 		
 	}
 
@@ -29,8 +33,23 @@ public class RecursiveMethods {
 	 * @param radiusMinimumDrawingThreshold radius above which drawing should occur
 	 */
 	public static void circlesUponCircles(double xCenter, double yCenter, double radius, double radiusMinimumDrawingThreshold) {
+		if (radius < radiusMinimumDrawingThreshold) {
+			return; 
+		}
+		StdDraw.circle(xCenter,yCenter,radius); 	
 		
+		circlesUponCircles(xCenter + radius, yCenter, radius/3, radiusMinimumDrawingThreshold);
+		circlesUponCircles(xCenter - radius, yCenter, radius/3, radiusMinimumDrawingThreshold);
+		circlesUponCircles(xCenter, yCenter + radius, radius/3, radiusMinimumDrawingThreshold);
+		circlesUponCircles(xCenter, yCenter - radius, radius/3, radiusMinimumDrawingThreshold);
+		
+	
 		// FIXME complete the recursive drawing
+		
+		
+		 //i dont want to do this at all
+		
+
 	}
 	
 
@@ -44,7 +63,32 @@ public class RecursiveMethods {
 		
 			// FIXME create a helper method that can recursively reverse the given array
 			return new int[0];
+	}
+	
+	public static int[] toReversedHelper(int[] array) {		
+		int[] reversed = new int[array.length];
 		
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] >= array.length) { //base case
+				return 0;
+		} else {
+			int odds = countOddHelper(values, index + 1); //recursive step
+			if(values[index] % 2 != 0) {
+				odds++;
+			}
+			return odds;
+		}
+			
+//			int[] reversed = new int[array.length];
+//			if (array.length > 0) {
+//				int lastIndex = array.length - 1;
+//				for (int index = 0; index <= array.length / 2; ++index) {
+//					int mirrorIndex = lastIndex - index;
+//					// note:
+//					// since we read from array and write to reversed
+//					// we do not need to use a temp variable
+//					reversed[index] = array[mirrorIndex];
+//					reversed[mirrorIndex] = array[index];
 	}
 
 	/**
